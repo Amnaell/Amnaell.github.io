148c3fabcad9a2950d4fd34a92aa1c39d011d686
Amnaell <erwan.hamzaoui51000@gmail.com>
2025-07-01 00:08:06 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -56,9 +56,7 @@ function renderUnits() {
     card.onclick = () => toggleUnit(unit.id);
 
     card.innerHTML = `
-      <img src="${unit.image}" alt="Unit ${unit.name}" class="w-full rounded mb-2" />
-      <div class="text-xs font-semibold">${unit.name}</div>
-      <div class="text-xs text-gray-400">${unit.attribute} • ${unit.rarity}</div>
+      <img src="${unit.image}" alt="${unit.name}" class="w-full rounded mb-2" />
     `;
 
     container.appendChild(card);
